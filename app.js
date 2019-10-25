f958fc0a1d9e6bd72cb573bf96dbadfe07cdb653
Quazirafi1 <55546196+Quazirafi1@users.noreply.github.com>
2019-10-26 01:59:51 +0600

    Bootstrap loaded
    Changed ide to WebStorm for easier bootstrap installation and also used handlebars as view engine instead of ejs

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
+/* Importing Different Modules */
+
 const express = require('express');
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-const path = require('path'); 
+const path = require('path');
+const hbs = require('express-handlebars');
+const {mongoDbUrl} = require('./config/configuration');
+
+
+
 const app = express();
 
-//Configure mongoose to connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true })
-	.then(response => {
-		console.log("MongoDB connected seccessfully");
-	}).catch(err => {
-		console.log("DB connection failed.");
-	})
 
-//configure express
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true})); 	
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Routes
-app.use('/', (req, res) => {
-	res.send("Welcome to the blog!");
+// Configure Mongoose to Connect to MongoDB
+mongoose.connect(mongoDbUrl, { useNewUrlParser: true })
+    .then(response => {
+        console.log("MongoDB Connected Successfully.");
+    }).catch(err => {
+        console.log("Database connection failed.");
 });
 
 
 
-//server listens at port 3000, server start-up
+/* Configure express*/
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/* Setup View Engine To Use Handlebars */
+app.engine('handlebars', hbs({defaultLayout: 'default'}));
+app.set('view engine' , 'handlebars');
+
+
+/* Routes */
+app.use('/', (req, res) => {
+   res.render('default/index');
+});
+
+
+
+
 app.listen(3000, () => {
-	console.log('server started at 3000...');
+    console.log(`Server is running on port 3000`);
 });
