0883b7a326a8156c35bb4aa55edcbcf0ff7eccbe
Quazi Ghulam Rafi <55546196+Quazirafi1@users.noreply.github.com>
2019-11-03 00:53:29 +0600

    login authentication successful

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 /* Importing Different Modules */
+
 const {globalVariables} = require('./config/configuration');
+
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const hbs = require('express-handlebars');
 const {mongoDbUrl, PORT} = require('./config/configuration');
 const flash = require('connect-flash');
-//const {PORT} = require('./config/configuration');
 const session = require('express-session');
 const methodOverride = require('method-override');
 const {selectOption} = require('./config/customFunctions');
 const fileUpload = require('express-fileupload');
+const passport = require('passport');
 
 const app = express();
 
@@ -30,26 +32,35 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* flash and session*/
+
+/*  Flash and Session*/
 app.use(session({
-        secret: 'anysecret',
-        saveUninitialized: true,
-        resave: true,
+    secret: 'anysecret',
+    saveUninitialized: true,
+    resave: true
 }));
 
 app.use(flash());
+
+/* Passport Initialize */
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Use Global Variables */
 app.use(globalVariables);
 
-/* file upload middleware*/
-app.use(fileUpload());
 
+/* File Upload Middleware*/
+app.use(fileUpload());
 
 /* Setup View Engine To Use Handlebars */
 app.engine('handlebars', hbs({defaultLayout: 'default', helpers: {select: selectOption}}));
 app.set('view engine' , 'handlebars');
 
-/* method override middleware */
+
+/* Method Override Middleware*/
 app.use(methodOverride('newMethod'));
+
 
 /* Routes */
 const defaultRoutes = require('./routes/defaultRoutes');
@@ -58,6 +69,8 @@ const adminRoutes = require('./routes/adminRoutes');
 app.use('/', defaultRoutes);
 app.use('/admin', adminRoutes);
 
+
+/* Start The Server */
 app.listen(PORT, () => {
-   console.log('Server is running on port '+PORT);
+    console.log(`Server is running on port ${PORT}`);
 });
--- defaultController.js
+++ defaultController.js
@@ -3,72 +3,79 @@ const Category = require('../models/CategoryModel').Category;
 const bcrypt = require('bcryptjs');
 const User = require('../models/UserModel').User;
 
+
 module.exports = {
-  index: async (req, res) => {
 
-    const posts = await Post.find();
-    const categories = await Category.find();
+    index: async (req, res) => {
 
-    res.render('default/index', {posts: posts, categories: categories});
-  },
-  loginGet: (req, res) => {
-    res.render('default/login');
-  },
-  loginPost: (req, res) => {
-    res.send('congrats login');
-  },
-  registerGet: (req, res) => {
-    res.render('default/register');
-  },
-  registerPost: (req, res) => {
-    //res.send('congrats registered');
-    let errors = [];
+        const posts = await Post.find();
+        const categories = await Category.find();
+        res.render('default/index', {posts: posts, categories: categories});
+    },
 
-    if(!req.body.firstName){
-      errors.push({message: 'First name is mandatory'});
-    }
+    /* LOGIN ROUTES */
+    loginGet: (req, res) => {
+        res.render('default/login', {message: req.flash('error')});
+    },
 
-    if(!req.body.lastName){
-      errors.push({message: 'Last name is mandatory'});
-    }
 
-    if(!req.body.email){
-      errors.push({message: 'Email is mandatory'});
-    }
+    loginPost: (req, res) => {
 
-    if(req.body.password !== req.body.passwordConfirm){
-      errors.push({message: 'Password do not match'});
-    }
-    if(errors.length > 0)
-    {
-      res.render('default/register', {
-        errors: errors,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email
+    },
 
-      });
-    }
-    else{
-      User.findOne({email: req.body.email}).then(user =>{
-        if(user){
-          req.flash('error-message', 'Email already exists, try to login');
-          res.redirect('/login');
+    /* REGISTER ROUTES*/
+
+    registerGet: (req, res) => {
+        res.render('default/register');
+    },
+
+    registerPost: (req, res) => {
+        let errors = [];
+
+        if (!req.body.firstName) {
+            errors.push({message: 'First name is mandatory'});
         }
-        else {
-          const newUser = new User(req.body);
+        if (!req.body.lastName) {
+            errors.push({message: 'Last name is mandatory'});
+        }
+        if (!req.body.email) {
+            errors.push({message: 'Email field is mandatory'});
+        }
+        if (!req.body.password || !req.body.passwordConfirm) {
+            errors.push({message: 'Password field is mandatory'});
+        }
+        if (req.body.password !== req.body.passwordConfirm) {
+            errors.push({message: 'Passwords do not match'});
+        }
 
-          bcrypt.genSalt(10, (err,salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              newUser.password = hash;
-              newUser.save().then(user => {
-                req.flash('success-message', 'You are now registered');
-                res.redirect('/login');
-              });
+        if (errors.length > 0) {
+            res.render('default/register', {
+                errors: errors,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email
             });
-          });
+        } else {
+            User.findOne({email: req.body.email}).then(user => {
+                if (user) {
+                    req.flash('error-message', 'Email already exists, try to login.');
+                    res.redirect('/login');
+                } else {
+                    const newUser = new User(req.body);
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            newUser.password = hash;
+                            newUser.save().then(user => {
+                                req.flash('success-message', 'You are now registered');
+                                res.redirect('/login');
+                            });
+                        });
+                    });
+                }
+            });
         }
-      })
     }
-  }
+
 };
+
